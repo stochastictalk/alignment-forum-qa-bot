6faa6e4ae8dd5e3924aa460712317341f30b0d62
chaosGuppy <lawphi56@gmail.com>
2023-02-11 18:32:03 +0000

    add per-author summerization

--- src/alignment_forum_qa_bot/_QABot.py
+++ src/alignment_forum_qa_bot/_QABot.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 import openai
@@ -17,7 +18,7 @@ class QABot:
         openai.api_key = openai_api_key
         self._post_retriever = KeywordSearchRetriever()
 
-    def query(self, query_text: str) -> str:
+    def query(self, query_text: str, method="summarize-first") -> str:
         """Asks a question about alignment forum's post corpus using OpenAI's completion API.
 
         Parameters
@@ -26,12 +27,51 @@ class QABot:
             The query input by the Alignment Forum user.
         """
         posts = self._post_retriever.retrieve(query=query_text)
-        return self._get_response(query_text, posts)
+        return self._get_response(query_text, posts, method=method)
 
     def _get_response(self, query_text: str, posts: List[RetrievedParagraph], method="summarize-first"):
-        case_switch = {"summarize-first": self._summarize_first}
+        case_switch = {
+            "summarize-first": self._summarize_first,
+            "summarize-by-author": self._summarize_by_author,
+        }
 
         return case_switch[method](query_text, posts)
+
+    def _summarize_by_author(self, query_text: str, posts: RetrievedParagraph):
+        posts_by_author = defaultdict(list)
+        for post in posts:
+            posts_by_author[post["author"]].append(post["paragraph"])
+        author_post_pairs = list(posts_by_author.items())
+        result = ""
+        for author, posts in author_post_pairs[:5]:
+            result += f"{author}'s views:\n\n"
+            prompt_for_author_posts = self._get_prompt_for_author_posts(posts, query_text)
+            completion = self._get_response_for_author_prompt(prompt_for_author_posts)
+            result += completion + "\n\n"
+        return result
+
+    def _get_prompt_for_author_posts(self, posts: list[str], query_text: str):
+        prompt = f"""
+        I'm going to show you some sample paragraphs written by a user on the Alignment Forum.
+        Please summarize the views of this author on {query_text} in less than 200 words.
+        """
+        for post in posts[:20]:
+            prompt += f"\n\n{post}\n\n"
+        prompt += "Answer:"
+        return prompt
+
+    def _get_response_for_author_prompt(self, author_prompt: str):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=author_prompt,
+            # temperature=0.8,
+            max_tokens=2000,
+            # top_p=1,
+            # frequency_penalty=0,
+            # presence_penalty=0,
+            # stop=["\n"],
+        )
+        return response["choices"][0]["text"]
 
     def _summarize_first(self, query_text: str, posts: RetrievedParagraph):
         """Maps posts and question to prompt."""
